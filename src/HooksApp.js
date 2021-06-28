f3ad25be167b006ff65804bd477f8dd03effff1f
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-28 19:11:23 -0400

    Primer commit de la app * Creacion de nuevo alayout principal * Creacion del componente para la navbar * Importacion de estilos de Bootstrap

---
+++ src/HooksApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "./components/Navbar";
+
+export const HooksApp = () => {
+  return (
+    <>
+      <Navbar />
+
+      <main className="container">
+        <h1>Hola mundo</h1>
+      </main>
+    </>
+  );
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <header>
+      <nav className="navbar navbar-light">
+        <div className="container">
+          <span className="navbar-brand mb-0 h1 text-white">Hooks App</span>
+        </div>
+      </nav>
+    </header>
+  );
+};
