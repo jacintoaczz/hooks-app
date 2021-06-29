58155511c7a34b4d15bb781f4c130383a06ac788
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-28 21:20:36 -0400

    Usando nuevo componente como layout prinicpal

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import reportWebVitals from "./reportWebVitals";
 
 // import { HooksApp } from "./HooksApp";
 // import { CounterApp } from "./components/01-useState/CounterApp";
-import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+// import { CounterWithCustomHook } from "./components/01-useState/CounterWithCustomHook";
+import { SimpleForm } from "./components/02-useEffect/SimpleForm.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CounterWithCustomHook />
+    <SimpleForm />
   </React.StrictMode>,
   document.getElementById("root")
 );
