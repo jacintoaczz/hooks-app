062600b85a22cff50f4d51f8331a2ff16196e43d
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-29 10:08:17 -0400

    Mejorando la logica del hook con el useRef

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const useFetch = (url) => {
+  const isMounted = useRef(true);
+
+  // Se usa este efecto para monitorear la referencia al componente en si, asi, cuando el componente se
+  // desmonte, podemos tener un valor con el cual aplicar cierta logica.
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+
   const [state, setState] = useState({
     data: null,
     loading: true,
@@ -17,7 +27,11 @@ export const useFetch = (url) => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        setState({ loading: false, error: null, data });
+        // Si el setState se llama cuando el componente esta desmontado la app puede crashear, dado
+        // que esto puede causar fugas de memoria entre otros errores.
+        if (isMounted.current) {
+          setState({ loading: false, error: null, data });
+        }
       });
   }, [url]);
 
