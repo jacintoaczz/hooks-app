df1b8adb93f1581177f2a5f34a0ab69fdd51d67c
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-30 14:10:49 -0400

    Añadiendo nav-links al componente

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = ({ title = "Hooks App" }) => {
   return (
     <header>
-      <nav className="navbar navbar-light mb-3">
+      <nav className="navbar navbar-light mb-3 navbar-expand-md">
         <div className="container">
-          <span className="navbar-brand mb-0 h1 text-white">{title}</span>
+          <span className="navbar-brand text-white">{title}</span>
+
+          {/* Collapse del navbar */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="./">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="./login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="./about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/* #Collapse del navbar */}
         </div>
       </nav>
     </header>
