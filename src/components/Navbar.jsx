3a551d1a48caea588662df1e6d32ba3a0a0379dc
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-29 20:18:43 -0400

    Añadida prop para tener un titulo dinamico

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const Navbar = () => {
+export const Navbar = ({ title = "Hooks App" }) => {
   return (
     <header>
-      <nav className="navbar navbar-light">
+      <nav className="navbar navbar-light mb-3">
         <div className="container">
-          <span className="navbar-brand mb-0 h1 text-white">Hooks App</span>
+          <span className="navbar-brand mb-0 h1 text-white">{title}</span>
         </div>
       </nav>
     </header>
