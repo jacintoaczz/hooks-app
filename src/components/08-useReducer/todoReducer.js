2b8abee9cfd7b746483937e9ceae6ea57c580117
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-30 08:48:37 -0400

    Implementado el reducer para togglear el done

--- src/components/08-useReducer/todoReducer.js
+++ src/components/08-useReducer/todoReducer.js
@@ -8,6 +8,19 @@ export const todoReducer = (state = [], action) => {
     case types.deleteTodo:
       return state.filter((todo) => todo.id !== action.payload);
 
+    case types.toggleTodo:
+      return state.map((todo) =>
+        todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+      );
+
+    // case types.toggleTodo:
+    //   return state.map((todo) => {
+    //     if (todo.id === action.payload) {
+    //       return { ...todo, done: !todo.done };
+    //     }
+    //     return todo;
+    //   });
+
     default:
       return state;
   }
