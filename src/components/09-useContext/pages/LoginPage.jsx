b5fdf091bd46d0a77b4b1c3be0b2f694e23b23a2
Jacinto Acosta <jacintoaczz@gmail.com>
2021-06-30 16:08:15 -0400

    Modificando el state del usuario

--- src/components/09-useContext/pages/LoginPage.jsx
+++ src/components/09-useContext/pages/LoginPage.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 export const LoginPage = () => {
-  return <div></div>;
+  const { setUser } = useContext(UserContext);
+  const loggedUser = {
+    id: 0,
+    name: "Jacinto Acosta",
+    email: "jacintoaczz@gmail.com",
+  };
+
+  return (
+    <>
+      <main className="container">
+        <h1>Login page</h1>
+        <hr className="divider" />
+
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => {
+            setUser(loggedUser);
+          }}
+        >
+          Login
+        </button>
+
+        <button
+          className="btn btn-outline-danger mx-3"
+          onClick={() => {
+            setUser({});
+          }}
+        >
+          Logout
+        </button>
+      </main>
+    </>
+  );
 };
